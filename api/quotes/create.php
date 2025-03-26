dc28e28156cc0fa4676cbe6834d825db545e8fc3
Devon Letendre <d_letendre@mail.fhsu.edu>
2025-03-26 17:03:04 -0400

    Updated

--- api/quotes/create.php
+++ api/quotes/create.php
@@ -1,75 +1,65 @@
 <?php
-header('Access-Control-Allow-Origin: *');
-header('Content-Type: application/json');
-header('Access-Control-Allow-Methods: POST');
-header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+    header('Access-Control-Allow-Methods: POST');
+    header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
 
-include_once '../../config/Database.php';
-include_once '../../models/Quote.php';
+    include_once '../../config/Database.php';
+    include_once '../../models/Quote.php';
 
-$database = new Database();
-$db = $database->connect();
+    // Create DB & connect
+    $database = new Database();
+    $db = $database->connect();
 
-$quote = new Quote($db);
+    // Create author object
+    $quote = new Quote($db);
 
-// Get raw posted data
-$data = json_decode(file_get_contents("php://input"));
+    // Get raw posted data
+    $data = json_decode(file_get_contents("php://input"));
 
-// Validate input
-if (empty($data->quote) || empty($data->author_id) || empty($data->category_id)) {
-    echo json_encode(['message' => 'Missing Required Parameters']);
-    exit();
-}
+    // Ensure we have the required parameters
+    if (empty($data->quote) || empty($data->author_id) || empty($data->category_id)) {
+        echo json_encode(['message' => 'Missing Required Parameters']);
+        exit();
+    }
 
-// Assign values
-$quote->quote = htmlspecialchars(strip_tags($data->quote));
-$quote->author_id = intval($data->author_id);
-$quote->category_id = intval($data->category_id);
+    // Assign values
+    $quote->quote = htmlspecialchars(strip_tags($data->quote));
+    $quote->author_id = intval($data->author_id);
+    $quote->category_id = intval($data->category_id);
 
-// Check if author_id exists
-include_once '../../models/Author.php';
-$author = new Author($db);
-$author->id = $quote->author_id;
+    // Ensure author_id exists
+    include_once '../../models/Author.php';
+    $author = new Author($db);
+    $author->id = $quote->author_id;
 
-/*
-if (!$author->exists2()) {
-    echo json_encode(['message' => 'author_id Not Found']);
-    exit();
-}
-    */
+    if (!$author->exists()) {
+        echo json_encode(['message' => 'author_id Not Found']);
+        exit();
+    }
 
-if (!$author->exists()) {
-    echo json_encode(['message' => 'author_id Not Found']);
-    exit();
-}
+    // Ensure category_id exists
+    include_once '../../models/Category.php';
+    $category = new Category($db);
+    $category->id = $quote->category_id;
 
-// Check if category_id exists
-include_once '../../models/Category.php';
-$category = new Category($db);
-$category->id = $quote->category_id;
+    if (!$category->exists()) {
+        echo json_encode(['message' => 'category_id Not Found']);
+        exit();
+    }
 
-/*
-if (!$category->exists2()) {
-    echo json_encode(['message' => 'category_id Not Found']);
-    exit();
-}
-*/
-
-if (!$category->exists()) {
-    echo json_encode(['message' => 'category_id Not Found']);
-    exit();
-}
-
-// Create quote
-$newQuoteId = $quote->create3();
-if ($newQuoteId) {
-    echo json_encode([
-        'id' => $newQuoteId,
-        'quote' => $quote->quote,
-        'author_id' => $quote->author_id,
-        'category_id' => $quote->category_id
-    ]);
-} else {
-    echo json_encode(['message' => 'Quote Not Created']);
-}
+    // Create quote
+    $newQuoteId = $quote->create3();
+    if ($newQuoteId) {
+        echo json_encode([
+            'id' => $newQuoteId,
+            'quote' => $quote->quote,
+            'author_id' => $quote->author_id,
+            'category_id' => $quote->category_id
+        ]);
+    } 
+    // Some other issue occured - send a message
+    else {
+        echo json_encode(['message' => 'Quote Not Created']);
+    }
 ?>
