7ed1d082435bc356070ff340ba2fde4d0416fe95
Devon Letendre <d_letendre@mail.fhsu.edu>
2025-03-26 16:36:23 -0400

    Updated

--- api/categories/update.php
+++ api/categories/update.php
@@ -1,42 +1,51 @@
 <?php
-header('Access-Control-Allow-Origin: *');
-header('Content-Type: application/json');
-header('Access-Control-Allow-Methods: PUT');
-header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
+        header('Access-Control-Allow-Origin: *');
+        header('Content-Type: application/json');
+        header('Access-Control-Allow-Methods: PUT');
+        header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
 
-include_once '../../config/Database.php';
-include_once '../../models/Category.php';
+        include_once '../../config/Database.php';
+        include_once '../../models/Category.php';
 
-// Instantiate DB & connect
-$database = new Database();
-$db = $database->connect();
+        // Create DB & connect
+        $database = new Database();
+        $db = $database->connect();
 
-// Instantiate category object
-$category = new Category($db);
+        // Create category object
+        $category = new Category($db);
 
-// Get raw posted data
-$data = json_decode(file_get_contents("php://input"));
+        // Get raw posted data
+        $data = json_decode(file_get_contents("php://input"));
 
-// Ensure required data is present
-if (isset($data->id) && isset($data->category) && !empty($data->id) && !empty($data->category)) {
-    $category->id = intval($data->id);
-    $category->category = htmlspecialchars(strip_tags($data->category));
+        // Ensure both ID and category are provided
+        if (isset($data->id) && isset($data->category) && !empty($data->id) && !empty($data->category)) {
+        
+            // Set ID and author to update
+            $category->id = intval($data->id);
+            $category->category = htmlspecialchars(strip_tags($data->category));
 
-    // Check if category exists before updating
-    if ($category->exists()) {
-        // Update category
-        if ($category->update()) {
-            echo json_encode(array(
-                'id' => $category->id,
-                'category' => $category->category
-            ));
-        } else {
-            echo json_encode(array('message' => 'Failed to update category.'));
+            // Check if category exists
+            if ($category->exists()) {
+                // Update category
+                if ($category->update()) {
+                    // Return the updated author in the response
+                    echo json_encode(array(
+                        'id' => $category->id,
+                        'category' => $category->category
+                    ));
+                } 
+                // Return failed to update message
+                else {
+                    echo json_encode(array('message' => 'Failed to update category.'));
+                }
+            } 
+            // Return category ID not found
+            else {
+                echo json_encode(array('message' => 'Category ID Not Found.'));
+            }
+        } 
+        // Return missing required parameters message
+        else {
+            echo json_encode(array('message' => 'Missing Required Parameters'));
         }
-    } else {
-        echo json_encode(array('message' => 'Category ID Not Found.'));
-    }
-} else {
-    echo json_encode(array('message' => 'Missing Required Parameters'));
-}
 ?>
