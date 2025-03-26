5babf76f71b3c31c5d946cab8ac1f485dd18574b
Devon Letendre <d_letendre@mail.fhsu.edu>
2025-03-26 19:36:49 -0400

    Updated

--- index.php
+++ index.php
@@ -9,3 +9,19 @@ print_r($_SERVER);
 echo '</pre>';
 
 phpinfo();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Midterm Project/title>
+</head>
+<body>
+    <h1>YEAH</h1>
+</body>
+</html>
