1ab35f82f3ece24be8b24c8e7b2c319d3ed6fcc8
Devon Letendre <d_letendre@mail.fhsu.edu>
2025-03-26 17:44:17 -0400

    Updated

--- api/quotes/index.php
+++ api/quotes/index.php
@@ -1,38 +1,39 @@
 <?php
-//Headers to deal with CORS -- not needed in CRUD files
-header('Access-Control-Allow-Origin: *');
-header('Content-Type: application/json');
-$method = $_SERVER['REQUEST_METHOD'];
-if ($method === 'OPTIONS') {
-    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
-    header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
-}
+    //Headers to deal with CORS
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+    $method = $_SERVER['REQUEST_METHOD'];
+    if ($method === 'OPTIONS') {
+        header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+        header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
+    }
 
-require_once '../../config/Database.php';
-require_once '../../models/Author.php';
-require_once '../../models/Category.php';
-require_once '../../models/Quote.php';
+    // Data models & DB connection class
+    require_once '../../config/Database.php';
+    require_once '../../models/Author.php';
+    require_once '../../models/Category.php';
+    require_once '../../models/Quote.php';
 
-// Database connection
-$database = new Database();
-$db = $database->connect();
+    // Create DB & connect
+    $database = new Database();
+    $db = $database->connect();
 
-// Instantiate Quote object
-$quote = new Quote($db);
+    // Route to the correct operation based on HTTP method
+    $quote = new Quote($db);
 
-if($method == "GET"){
-    require_once 'read.php';
-}
+    if($method == "GET"){
+        require_once 'read.php';
+    }
 
-if ($method == "POST"){
-    require_once 'create.php';
-}
+    if ($method == "POST"){
+        require_once 'create.php';
+    }
 
-if ($method == "PUT"){
-    require_once 'update.php';
-}
+    if ($method == "PUT"){
+        require_once 'update.php';
+    }
 
-if ($method == "DELETE"){
-    require_once 'delete.php';
-}
+    if ($method == "DELETE"){
+        require_once 'delete.php';
+    }
 ?>
